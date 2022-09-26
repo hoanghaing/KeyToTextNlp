d4ce36b9c805a05a870d2f4e48e9a2911b7b261a
hainh43 <hoanghai18497@gmail.com>
2022-09-26 18:00:34 +0700

    fastAPI first version...

--- generate.py
+++ generate.py
@@ -1,4 +1,11 @@
 from transformers import pipeline
+import re
 generator = pipeline('text-generation', model='./gpt-neo-125M')
-x = generator("Korean idol singer", do_sample=True, max_new_tokens=60)
-print(x)
+# x = generator("Korean idol singer", do_sample=True, max_new_tokens=60)
+x = generator("Tommy shaw was", do_sample=True, max_new_tokens=60)
+z = re.sub(r"[\n\t]*", "", x[0]['generated_text'])
+# print(x[0]['generated_text'])
+print(z)
+
+# Tommy shaw was with me that day as I got on my stomach. I wanted to make a stop before the tour bus began to pull up to the front steps,
+# which was the last stop I had before the train left. The car doors opened. My eyes snapped up, and a familiar name came to mind
--- main_api.py
+++ main_api.py
@@ -1,15 +1,22 @@
-# pip install fastapi uvicorn asyncio pydantic typing
+# pip install fastapi uvicorn asyncio pydantic typing yake
 # python3 -m uvicorn main_api:app --reload
 # Importing Necessary modules
+from operator import le
 from fastapi import FastAPI
 import uvicorn
 import asyncio
 from pydantic import BaseModel
 from typing import (List)
+from googletrans import Translator
+from transformers import pipeline
+import re
+import yake
 
 # Declaring our FastAPI instance
 app = FastAPI()
- 
+translator = Translator() 
+kwExtractor = yake.KeywordExtractor()
+generator = pipeline('text-generation', model='./gpt-neo-125M')
 # Declaring the request sample Modal
 class request(BaseModel):
   titles: List[str] = None
@@ -19,15 +26,33 @@ def main():
     return { 'message': 'Welcome to HaiNH Intelligent API!' }
  
 @app.post('/autofill')
-def autofill(data: request):
+async def autofill(data: request):
   titles = data.titles
+  titlesToEnglish = translator.translate(titles, dest='en')
   paragraph = ''
-  for title in titles:
-    paragraph += f"{title}. "
-  print(paragraph)
+  for translated in titlesToEnglish:
+    if (translated.text):
+      plainText = re.sub(r'[^\w]', ' ', translated.text).strip()
+      paragraph += f"{plainText}. "   
+  keywords = kwExtractor.extract_keywords(paragraph)
+  candidateKeyword = ''
+  generateDescription = ''
+  if (len(keywords) > 0):
+    candidateKeyword = keywords[0][0]
+    if (candidateKeyword.find("is") == -1):
+      candidateKeyword += ' is'
+      candidateKeyword = "%s%s" % (candidateKeyword[0].upper(), candidateKeyword[1:]) #upper case first letter
+    descript = generator(candidateKeyword, do_sample = True, max_new_tokens = 60)
+    if (len(descript) > 0):
+      generateDescription = descript[0]['generated_text']
+      # generateDescription = generateDescription.strip('\n')
+      # generateDescription = generateDescription.strip('\t')
+      # generateDescription = re.sub('\s+', ' ', generateDescription)
+      generateDescription.rsplit(' ', 1)[0] # remove last word
+      generateDescription+='...'
   return {
-    'candidate': 'textIsHere',
-    'description': 'thisIsDescription'
+    'candidate': candidateKeyword,
+    'description': generateDescription,
   }
 if __name__ == "__main__":
     uvicorn.run("main_api:app", port=5077, log_level="info")
